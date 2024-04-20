90c790bc29cd31891918181bb224cf7d0def66cd
nongdamgomm <shn0322@kakao.com>
2024-04-20 14:30:35 +0900

    naver openapi를 이용하여 상품 불러온뒤, json파일로 저장 jsonserver upload, fastapi test

---
+++ fastapi/main.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python
+#!usr/bin/env python
+import uvicorn
+from fastapi import FastAPI
+app = FastAPI()
+import requests
+@app.get("/")
+def read_root():
+    return {"Hello":"World"}
+
+base_url = 'http://localhost:5000/items'
+@app.get(path='/items')
+def getItems():
+    reponse = requests.get(base_url)
+    return reponse.json()
+
+
+if __name__=='__main__':
+    uvicorn.run(app, host="0.0.0.0", port=8000)
